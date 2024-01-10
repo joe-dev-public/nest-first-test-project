884f29881550b15c10da35f350e68bc03869fb8e
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-10 17:55:35 +0000

    Use CRUD generator to add items entity (REST)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReleasesModule } from './releases/releases.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ReleasesModule],
+  imports: [ReleasesModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })

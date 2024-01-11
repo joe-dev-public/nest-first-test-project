d10edecea470801e65095012e5b951541e1a432f
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-11 18:16:40 +0000

    Releases service: placeholder data & basic 'get all' method

--- src/releases/releases.service.ts
+++ src/releases/releases.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ReleasesService {}
+export class ReleasesService {
+  // Placeholder data store
+  private releases = [
+    { id: 0, name: 'Left Leg Out / Blue Notez', year: 2006 },
+    { id: 1, name: 'Best Friend', year: 2002 },
+  ];
+
+  getReleases(year?: string) {
+    if (year) {
+      // Could just store the years as strings above, but I'd rather convert
+      // here so that the returned data feels more correct. (URL query
+      // strings are always strings.)
+      return this.releases.filter((release) => String(release.year) === year);
+    }
+
+    return this.releases;
+  }
+}

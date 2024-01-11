f2a6f1146236f8ffd19f53a7055857beb490303d
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-11 17:01:11 +0000

    Basic POST handler, including create DTO

---
+++ src/releases/dto/create-release.dto.ts
@@ -0,0 +1,3 @@
+export class CreateReleaseDto {
+  name: string;
+}
--- src/releases/releases.controller.ts
+++ src/releases/releases.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateReleaseDto } from './dto/create-release.dto';
 
 @Controller('releases')
 export class ReleasesController {
@@ -11,6 +12,13 @@ export class ReleasesController {
   getOneRelease(@Param('id') id: string) {
     return {
       id,
+    };
+  }
+
+  @Post()
+  createRelease(@Body() createReleaseDto: CreateReleaseDto) {
+    return {
+      name: createReleaseDto.name,
     };
   }
 }

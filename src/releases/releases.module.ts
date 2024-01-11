448de53095cf435c7bde3a0d253ea18dd6cf78a7
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-01-11 17:13:17 +0000

    Run linter

--- src/releases/releases.module.ts
+++ src/releases/releases.module.ts
@@ -4,6 +4,6 @@ import { ReleasesService } from './releases.service';
 
 @Module({
   controllers: [ReleasesController],
-  providers: [ReleasesService]
+  providers: [ReleasesService],
 })
 export class ReleasesModule {}

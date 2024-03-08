0d32a0f126528eee6ebb9681c40bffbc9e56f1f1
joe <94115299+joe-dev-public@users.noreply.github.com>
2024-03-08 23:43:02 +0000

    Port 8000 (same as FastAPI); enable CORS for local dev

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// const corsConfig = {
+//   origin: 'http://localhost:5173',
+//   // methods: 'GET,POST',
+// };
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // app.enableCors(corsConfig);
+  // Apparently without additional config, GET from localhost:5173 works? :¬/
+  // (Possibly because I'm fetching from localhost:8000 in the FE, rather
+  // than 127.0.0.1?
+  app.enableCors();
+  await app.listen(8000);
 }
 bootstrap();
